f9e57e8a4a80da47e529a84bafb6fa53d4e9abd5
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2023-12-31 13:52:29 +0300

    background

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <h1>app</h1>
+    <h1 className='text-center'>app</h1>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Welcome from './pages/Welcome/Welcome';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
-  
- 
+  <BrowserRouter>
+   
+    <Welcome/>
+  </BrowserRouter>
 );
 
 
