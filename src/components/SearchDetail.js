04ee145e29aaccf830f7ab106692cd6e92af14fb
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2024-01-01 18:38:58 +0300

    recipe book

---
+++ src/components/SearchDetail.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const SearchDetail = ({ item }) => {
+  return (
+    <div className="rounded overflow-hidden w-[400px] h-[550px] shadow-lg flex flex-col bg-orange-200">
+      <div className="relative">
+        <img
+          className="w-full"
+          src={item.recipe.image}
+          alt="Sunset in the mountains"
+        />
+        <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25"></div>
+
+        <div className="text-xs capitalize absolute rounded-lg top-0 right-0 bg-indigo-600 px-4 py-2 text-white mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out font-semibold">
+          {item.recipe.cuisineType}
+        </div>
+      </div>
+      <div className="px-6 py-4 font-semibold mb-auto flex flex-col">
+        <span>{item.recipe.label}</span>
+      </div>
+      <div className="px-6 py-3 flex flex-row items-center justify-between bg-gray-100">
+        <span
+          href="#"
+          className="py-1 text-xs font-regular text-gray-900 mr-1 flex flex-row items-center"
+        >
+          <span className="ml-1">Source : {item.recipe.source}</span>
+        </span>
+        <span
+          href="#"
+          className="py-1 text-xs font-regular text-gray-900 mr-1 flex flex-row items-center"
+        >
+          <svg
+            className="h-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+            ></path>
+          </svg>
+          <span className="ml-1">{item.recipe.totalTime} Comments</span>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default SearchDetail;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,70 +1,111 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import SearchDetail from "../components/SearchDetail";
+import "./Home.css"
 
 const Home = () => {
   const [select, setSelect] = useState("");
   const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(search, select);
+    getRecipe();
   };
 
+  const apiKey = "2e43178cb3e20d2b429690dfb240ec95";
+  const applicationId = "59b63c92";
+
+  const getRecipe = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.edamam.com/search?q=${search}&app_id=${applicationId}&app_key=${apiKey}&mealType=${select}`
+      );
+      setData(response.data.hits);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getRecipe();
+  }, []);
+  console.log(data);
+
   return (
-    <form onSubmit={handleSubmit} className="container w-[50%] mx-auto mt-5 ">
-      <div className="flex">
-        <label
-          htmlFor="location-search"
-          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-        >
-          Your Email
-        </label>
-
-        <div className=" w-full flex gap-2">
-          <select
-            value={select}
-            onChange={(e) => setSelect(e.target.value)}
-            id="countries"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    <div
+      className={
+        data.length > 1
+          ? "bg-gradient-to-r from-rose-400 to-orange-300 h-min-[100vh] "
+          : "home"
+      }
+    >
+      <form onSubmit={handleSubmit} className="container w-[50%] mx-auto pt-5 ">
+        <div className="flex">
+          <label
+            htmlFor="location-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
           >
-            <option value="Breakfast">Breakfast</option>
-            <option value="Lunch">Lunch</option>
-            <option value="Brunch">Brunch</option>
-            <option value="Dinner">Dinner</option>
-            <option value="Teatime">Teatime</option>
-          </select>
-          <input
-            type="search"
-            id='location-search"'
-            className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg   border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-            placeholder="Search for city or address"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            required=""
-          />
+            Your Email
+          </label>
 
-          <button
-            type="submit"
-            className=" w-[100px] flex items-center gap-2 h-full p-2.5 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
-          >
-            <svg
-              className="w-4 h-4"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
+          <div className=" w-full flex gap-2">
+            <select
+              value={select}
+              onChange={(e) => setSelect(e.target.value)}
+              id="countries"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-            <span className="">Search</span>
-          </button>
+              <option value="breakfast">Breakfast</option>
+              <option value="Lunch">Lunch</option>
+              <option value="Brunch">Brunch</option>
+              <option value="Dinner">Dinner</option>
+              <option value="Teatime">Teatime</option>
+            </select>
+            <input
+              type="search"
+              id='location-search"'
+              className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg   border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+              placeholder="Search for city or address"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              required=""
+            />
+
+            <button
+              type="submit"
+              className=" w-[100px] flex items-center gap-2 h-full p-2.5 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
+            >
+              <svg
+                className="w-4 h-4"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+              <span className="">Search</span>
+            </button>
+          </div>
         </div>
+      </form>
+      <div
+        class=" p-5 flex flex-wrap  gap-4 
+      justify-center items-start
+      "
+      >
+        {data.length > 1 &&
+          data.map((item) => (
+            <SearchDetail key={item.recipe.calories} item={item} />
+          ))}
       </div>
-    </form>
+    </div>
   );
 };
 
