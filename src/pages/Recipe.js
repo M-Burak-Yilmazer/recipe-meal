88b7a762f0e92f19f4f926072faca62e2ed1ccdd
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2024-01-01 19:57:18 +0300

    label

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "./recipe.css";
+import { useLocation } from "react-router-dom";
 
 const Recipe = () => {
+  const location = useLocation();
+  const recipe = location.state.from.recipe
+  console.log(recipe)
   return (
-    <div className="bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 recipe"></div>
+    <div className="bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 recipe">
+      <div className="recipeDiv w-[90vw] md:w-[50wv]  mx-auto h-screen">
+        <h1 className="text-2xl md:text-4xl font-semibold capitalize">{recipe.label}</h1>
+        
+      </div>
+    </div>
   );
 };
 
