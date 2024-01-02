a3f0b9b30b9bb43f27d9cca1c9f2b3f08277b488
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2024-01-02 03:48:59 +0300

    opacity

--- src/components/SearchDetail.js
+++ src/components/SearchDetail.js
@@ -16,7 +16,7 @@ const SearchDetail = ({ item }) => {
         />
         <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25"></div>
 
-        <div className="text-xs capitalize absolute rounded-lg top-0 right-0 bg-indigo-600 px-4 py-2 text-white mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out font-semibold">
+        <div className="text-xs capitalize absolute rounded-lg top-0 right-0 bg-orange-500 opacity-60 px-4 py-2 text-white mt-3 mr-3 hover:bg-white hover:text-orange-500 transition duration-500 ease-in-out font-semibold">
           {item.recipe.cuisineType}
         </div>
       </div>
